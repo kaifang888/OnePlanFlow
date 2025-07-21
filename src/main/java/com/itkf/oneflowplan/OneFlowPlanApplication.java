f669aa6dd91b8bedf008a2b572212539c8a17553
love_like_quick_sand <kaifang7623669@163.com>
2025-07-22 01:28:06 +0800

    初始工程的搭建

---
+++ src/main/java/com/itkf/oneflowplan/OneFlowPlanApplication.java
@@ -0,0 +1,13 @@
+package com.itkf.oneflowplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneFlowPlanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneFlowPlanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itkf/oneflowplan/OneFlowPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itkf.oneflowplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneFlowPlanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
